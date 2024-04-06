1b667a564babd6a5f6549037cd89ff8d405672a5
DarilRaf <darilmrafansyah@gmail.com>
2024-04-06 14:20:15 +0700

    initialize

---
+++ src/test/java/id/ac/ui/cs/advprog/farrel/FarrelApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.farrel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarrelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
