b7b1f927fe598292f19e1487dfe581f08d0662c4
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-04-17 17:24:14 +0700

    [REFACTOR] Changed class name to TopUpIteratorTest

--- src/test/java/id/ac/ui/cs/advprog/farrel/util/TopUpIteratorTest.java
+++ src/test/java/id/ac/ui/cs/advprog/farrel/util/TopUpIteratorTest.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.UUID;
 
-class PaymentIteratorImplTest {
+class TopUpIteratorTest {
     private List<TopUp> topUps;
-    private PaymentIteratorImpl paymentIterator;
+    private TopUpIterator topUpIterator;
 
     @BeforeEach
     public void setUp() {
@@ -17,12 +17,12 @@ class PaymentIteratorImplTest {
         topUps.add(new TopUp(UUID.randomUUID(), "PENDING"));
         topUps.add(new TopUp(UUID.randomUUID(), "ACCEPTED"));
         topUps.add(new TopUp(UUID.randomUUID(), "PENDING"));
-        paymentIterator = new PaymentIteratorImpl(topUps);
+        topUpIterator = new TopUpIterator(topUps);
     }
 
     @Test
     public void testFindByStatusPending() {
-        ArrayList<TopUp> pendingTopUps = paymentIterator.findByStatus("PENDING");
+        ArrayList<TopUp> pendingTopUps = topUpIterator.findByStatus("PENDING");
         assertEquals(2, pendingTopUps.size());
         assertEquals("PENDING", pendingTopUps.get(0).getStatus());
         assertEquals("PENDING", pendingTopUps.get(1).getStatus());
@@ -30,7 +30,7 @@ class PaymentIteratorImplTest {
 
     @Test
     public void testFindByStatusNotPending() {
-        ArrayList<TopUp> nonPendingTopUps = paymentIterator.findNotByStatus("PENDING");
+        ArrayList<TopUp> nonPendingTopUps = topUpIterator.findNotByStatus("PENDING");
         assertEquals(1, nonPendingTopUps.size());
         assertEquals("ACCEPTED", nonPendingTopUps.get(0).getStatus());
     }
@@ -38,7 +38,7 @@ class PaymentIteratorImplTest {
     @Test
     public void testFindByIdFound() {
         UUID id = topUps.get(0).getId();
-        TopUp foundTopUp = (TopUp) paymentIterator.findById(id);
+        TopUp foundTopUp = (TopUp) topUpIterator.findById(id);
         assertNotNull(foundTopUp);
         assertEquals(id, foundTopUp.getId());
     }
@@ -46,7 +46,7 @@ class PaymentIteratorImplTest {
     @Test
     public void testFindByIdNotFound() {
         UUID id = UUID.randomUUID();
-        TopUp foundTopUp = (TopUp) paymentIterator.findById(id);
+        TopUp foundTopUp = (TopUp) topUpIterator.findById(id);
         assertNull(foundTopUp);
     }
 }
