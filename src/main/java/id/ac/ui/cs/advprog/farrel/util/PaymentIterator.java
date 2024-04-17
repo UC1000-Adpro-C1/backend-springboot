d2fa4758f02f710e9fdf9636d1f863ffc1ad9dc2
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-04-17 19:40:51 +0700

    [GREEN] Implement PaymentIterator

---
+++ src/main/java/id/ac/ui/cs/advprog/farrel/util/PaymentIterator.java
@@ -0,0 +1,54 @@
+package id.ac.ui.cs.advprog.farrel.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class PaymentIterator {
+    private List<Payment> payments;
+    private int position = 0;
+
+    public PaymentIterator(List<Payment> payments) {
+        this.payments = payments;
+    }
+
+    public boolean hasNext() {
+        return position < payments.size();
+    }
+
+    public Payment next() {
+        if (hasNext()) {
+            return payments.get(position++);
+        }
+        return null;
+    }
+
+    public List<Payment> findByStatus(String status) {
+        List<Payment> result = new ArrayList<Payment>();
+        for (Payment payment : payments) {
+            if (payment.getStatus().equals(status)) {
+                result.add(payment);
+            }
+        }
+        return result;
+    }
+
+    public List<Payment> findNotByStatus(String status) {
+        List<Payment> result = new ArrayList<TopUp>();
+        for (Payment payment : payments) {
+            if (!payment.getStatus().equals(status)) {
+                result.add(payment);
+            }
+        }
+        return result;
+    }
+
+    public TopUp findById(UUID id) {
+        for (TopUp topUp : topUps) {
+            if (topUp.getId().equals(id)) {
+                return topUp;
+            }
+        }
+        return null;
+    }
+}
