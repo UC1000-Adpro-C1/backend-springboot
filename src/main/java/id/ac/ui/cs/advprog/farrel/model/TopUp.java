b1dd39ed4ce1ce60ff931b3940410950f7568661
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-05-08 14:33:11 +0700

    [REFACTOR] add new attr in topup

--- src/main/java/id/ac/ui/cs/advprog/farrel/model/TopUp.java
+++ src/main/java/id/ac/ui/cs/advprog/farrel/model/TopUp.java
@@ -12,6 +12,31 @@ public class TopUp {
     private long amount;
     private String status = TopUpStatus.PENDING.name();
     private LocalDate transactionTime;
-    private ArrayList<User> observerUser = new ArrayList<User>();
+    private ArrayList<User> observerUsers = new ArrayList<User>();
     private UUID topUpId;
+    private String userOwnerId;
+
+    public TopUp () {};
+
+    public TopUp (UUID topUpId, ArrayList<User> observerUsers, long amount, LocalDate transactionTime, String userOwnerId) {
+        this.topUpId = topUpId;
+        this.transactionTime = transactionTime;
+        this.status = TopUpStatus.PENDING.getValue();
+        this.userOwnerId = userOwnerId;
+
+        if (observerUsers.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.observerUsers = observerUsers;
+        }
+    }
+
+    public void setStatus(String status) {
+        if (TopUpStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
