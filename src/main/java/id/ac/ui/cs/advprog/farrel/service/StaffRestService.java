483a11aead2d9c6197fd823cbb0ce7a900b06420
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-05-08 14:11:17 +0700

    [GREEN] implement the new staffrestservice

--- src/main/java/id/ac/ui/cs/advprog/farrel/service/StaffRestService.java
+++ src/main/java/id/ac/ui/cs/advprog/farrel/service/StaffRestService.java
@@ -3,8 +3,8 @@ package id.ac.ui.cs.advprog.farrel.service;
 import id.ac.ui.cs.advprog.farrel.enums.TopUpStatus;
 import id.ac.ui.cs.advprog.farrel.model.Staff;
 import id.ac.ui.cs.advprog.farrel.model.TopUp;
-import id.ac.ui.cs.advprog.farrel.repository.StaffRepository;
 import id.ac.ui.cs.advprog.farrel.util.TopUpIterator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,38 +14,36 @@ import java.util.UUID;
 public class StaffRestService {
 
     private final TopUpIterator topUpIterator;
-    private final StaffRepository staffRepository;
 
-    public StaffRestService(TopUpIterator topUpIterator, StaffRepository staffRepository) {
+    @Autowired
+    public StaffRestService(TopUpIterator topUpIterator) {
         this.topUpIterator = topUpIterator;
-        this.staffRepository = staffRepository;
     }
 
-    public List<TopUp> getTopUpHistory() {
-        return topUpIterator.findNotByStatus("PENDING");
-    }
-
-    public List<TopUp> getOnGoingTopUp() {
-        return topUpIterator.findByStatus("PENDING");
-    }
-
-    public boolean checkIsStaff(String staffUsername) {
-        Staff staff = staffRepository.findByUserId(staffUsername);
-
-        return staff != null;
-    }
-
-    public void updateTopUpStatus(String staffUsername, UUID topUpId, String status) {
-        if (checkIsStaff(staffUsername)) {
-            TopUp topUp = topUpIterator.findById(topUpId);
-
-            if (topUp != null) {
-                if (status.equals(TopUpStatus.SUCCESS.name()))
-                    topUp.setStatus(TopUpStatus.SUCCESS.name());
-                else topUp.setStatus(TopUpStatus.FAILED.name());
-
-                topUpIterator.save(topUp);
-            }
+    public TopUp updateTopUpStatus(UUID id, TopUpStatus status, Staff staff) {
+        TopUp topUp = topUpIterator.findById(id);
+        if (topUp == null) {
+            throw new IllegalArgumentException("TopUp not found: " + id);
         }
+
+        if (!staff.isStaff()) {
+            throw new IllegalArgumentException("Unauthorized access");
+        }
+
+        if (!topUp.getStatus().equals(TopUpStatus.PENDING.name())) {
+            throw new IllegalArgumentException("TopUp status is not PENDING");
+        }
+
+        topUp.setStatus(status.name());
+        topUpIterator.save(topUp);
+        return topUp;
+    }
+
+    public List<TopUp> getPendingTopUps() {
+        return topUpIterator.findByStatus(TopUpStatus.PENDING.name());
+    }
+
+    public List<TopUp> getNonPendingTopUps() {
+        return topUpIterator.findNotByStatus(TopUpStatus.PENDING.name());
     }
 }
