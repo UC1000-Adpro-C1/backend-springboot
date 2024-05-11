2364e4f72bc14ee7471657b46a880918d77b12ba
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-05-12 01:12:54 +0700

    [GREEN] Implement staff topup controller

---
+++ src/main/java/id/ac/ui/cs/advprog/farrel/controller/StaffRestController.java
@@ -0,0 +1,50 @@
+package id.ac.ui.cs.advprog.farrel.controller;
+
+import id.ac.ui.cs.advprog.farrel.model.TopUp;
+import id.ac.ui.cs.advprog.farrel.service.StaffRestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class StaffRestController {
+
+    @Autowired
+    private StaffRestService topUpService;
+
+    @PostMapping("/topup")
+    public ResponseEntity<TopUp> createTopUp(@RequestBody TopUp topUp) {
+        TopUp createdTopUp = topUpService.createTopUp(topUp);
+        return new ResponseEntity<>(createdTopUp, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/topups")
+    public ResponseEntity<List<TopUp>> getAllTopUps() {
+        List<TopUp> allTopUps = topUpService.findAll();
+        return new ResponseEntity<>(allTopUps, HttpStatus.OK);
+    }
+
+    @GetMapping("/topup/{id}")
+    public ResponseEntity<TopUp> getTopUpById(@PathVariable("id") UUID id) {
+        try {
+            TopUp topUp = topUpService.findById(id);
+            return new ResponseEntity<>(topUp, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Id Listing " + id + " not found");
+        }
+    }
+
+    @PutMapping("/topup/{id}/update-status")
+    public ResponseEntity<TopUp> updateStatus(@PathVariable("id") UUID id, @RequestParam("status") String newStatus) {
+        TopUp updatedTopUp = topUpService.updateStatus(id, newStatus);
+        return new ResponseEntity<>(updatedTopUp, HttpStatus.OK);
+    }
+}
