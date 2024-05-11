d9fa2f51534074e103e7eca710b1169d1a816d93
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-05-12 01:57:27 +0700

    [REFACTOR] added more methods to repository

--- src/main/java/id/ac/ui/cs/advprog/farrel/repository/StaffTopUpRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/farrel/repository/StaffTopUpRepository.java
@@ -1,8 +1,18 @@
 package id.ac.ui.cs.advprog.farrel.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import id.ac.ui.cs.advprog.farrel.model.TopUp;
+
+import java.util.List;
+
 @Repository
 public interface StaffTopUpRepository extends JpaRepository<TopUp, String> {
+    @Query("SELECT t FROM TopUp t WHERE t.status = :status")
+    List<TopUp> findByStatus(@Param("status") String status);
+
+    @Query("SELECT t FROM TopUp t WHERE t.status <> :status")
+    List<TopUp> findByStatusNot(@Param("status") String status);
 }
