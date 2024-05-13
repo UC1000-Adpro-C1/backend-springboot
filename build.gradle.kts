e1b7954f48c49c711d3eb67620784c90d5697699
kenishajazlyn <124899946+KenishaJazlyn@users.noreply.github.com>
2024-05-13 14:04:48 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,18 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
+  	id("org.sonarqube") version "4.4.1.3373"
 }
+
+sonar {
+  properties {
+    property("sonar.projectKey", "UC1000-Adpro-C1_backend-springboot")
+    property("sonar.organization", "uc1000-adpro-c1")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
+}
+
+
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
