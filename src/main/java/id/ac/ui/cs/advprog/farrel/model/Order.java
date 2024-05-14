99fa6cbac2d019b81760803470b8c71d5241a42c
Samuelwidjaja <james.samuel.widjaja@gmail.com>
2024-05-15 00:22:01 +0700

    [GREEN] Implement Order Model

---
+++ src/main/java/id/ac/ui/cs/advprog/farrel/model/Order.java
@@ -0,0 +1,68 @@
+package id.ac.ui.cs.advprog.farrel.model;
+
+import id.ac.ui.cs.advprog.farrel.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Getter @Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String itemId;
+    private String status;
+    private String buyerId;
+
+    public Order(){
+
+    }
+    public Order(Order.OrderBuilder builder){
+        this.id = UUID.randomUUID().toString();
+        this.itemId = builder.itemId;
+        this.status = builder.status;
+        this.buyerId = builder.buyerId;
+    }
+
+    public void setStatus(String status) {
+        if(OrderStatus.contains(status)){
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static class OrderBuilder {
+        // Required parameters
+        private String itemId;
+
+        // Optional parameters
+        private String status;
+        private String buyerId;
+
+        public OrderBuilder(String itemId) {
+            this.itemId = itemId;
+            this.status = OrderStatus.WAITING_PAYMENT.getValue();
+        }
+
+        public Order.OrderBuilder setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Order.OrderBuilder setBuyerId(String buyerId) {
+            this.buyerId = buyerId;
+            return this;
+        }
+
+        public Order build() {
+            return new Order(this);
+        }
+    }
+}
