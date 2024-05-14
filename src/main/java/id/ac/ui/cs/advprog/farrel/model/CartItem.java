be90211ca8b6bb0feafb858abb9c0ab4d31eca87
DarilRaf <darilmrafansyah@gmail.com>
2024-05-14 17:01:42 +0700

    [REFACTOR] Added a price setter for the updatePrice feature from service

--- src/main/java/id/ac/ui/cs/advprog/farrel/model/CartItem.java
+++ src/main/java/id/ac/ui/cs/advprog/farrel/model/CartItem.java
@@ -26,4 +26,12 @@ public class CartItem {
             this.quantity = quantity;
         }
     }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            this.price = 0;
+        } else {
+            this.price = price;
+        }
+    }
 }
