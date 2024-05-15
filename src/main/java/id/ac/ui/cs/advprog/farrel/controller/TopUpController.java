1f9758a0c9b7e92ec5b1b924c5175119bb7f16d4
febrian-irv <123155363+febrian-irv@users.noreply.github.com>
2024-05-15 19:40:51 +0700

    [REFACTOR] correcting partition between TopUp and StaffRest Controller

--- src/main/java/id/ac/ui/cs/advprog/farrel/controller/TopUpController.java
+++ src/main/java/id/ac/ui/cs/advprog/farrel/controller/TopUpController.java
@@ -27,49 +27,9 @@ public class TopUpController {
         return new ResponseEntity<>(createdTopUp, HttpStatus.CREATED);
     }
 
-    @GetMapping("/topups")
-    public ResponseEntity<List<TopUp>> getAllTopUps() {
-        List<TopUp> allTopUps = topUpService.findAllTopUps();
-        return new ResponseEntity<>(allTopUps, HttpStatus.OK);
-    }
-
-    @GetMapping("/topup/{id}")
-    public ResponseEntity<TopUp> getTopUpById(@PathVariable("id") UUID id) {
-        try {
-            TopUp topUp = topUpService.findTopUpById(id);
-            return new ResponseEntity<>(topUp, HttpStatus.OK);
-        } catch (NoSuchElementException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Id TopUp " + id + " not found");
-        }
-    }
     @GetMapping("/topups/{userOwnerId}")
     public ResponseEntity<List<TopUp>> getTopUpByUserId(@PathVariable("userOwnerId") String userOwnerId) {
         List<TopUp> userTopUps = topUpService.findTopUpByUserId(userOwnerId);
         return new ResponseEntity<>(userTopUps, HttpStatus.OK);
     }
-
-            @GetMapping("/topups/pending")
-    public ResponseEntity<List<TopUp>> getPendingTopUps() {
-        List<TopUp> pendingTopUps = topUpService.findTopUpByStatus(TopUpStatus.PENDING.name());
-        return new ResponseEntity<>(pendingTopUps, HttpStatus.OK);
-    }
-
-    @GetMapping("/topups/non-pending")
-    public ResponseEntity<List<TopUp>> getNonPendingTopUps() {
-        List<TopUp> nonPendingTopUps = topUpService.findTopUpByStatusNot(TopUpStatus.PENDING.name());
-        return new ResponseEntity<>(nonPendingTopUps, HttpStatus.OK);
-    }
-
-    @PutMapping("/topup/{id}/update-status/success")
-    public ResponseEntity<TopUp> updateTopUpStatusToSuccess(@PathVariable("id") UUID id) {
-        TopUp updatedTopUp = topUpService.updateTopUpStatus(id, TopUpStatus.SUCCESS.name());
-        return new ResponseEntity<>(updatedTopUp, HttpStatus.OK);
-    }
-
-    @PutMapping("/topup/{id}/update-status/failed")
-    public ResponseEntity<TopUp> updateTopUpStatusToFailed(@PathVariable("id") UUID id) {
-        TopUp updatedTopUp = topUpService.updateTopUpStatus(id, TopUpStatus.FAILED.name());
-        return new ResponseEntity<>(updatedTopUp, HttpStatus.OK);
-    }
-
 }
