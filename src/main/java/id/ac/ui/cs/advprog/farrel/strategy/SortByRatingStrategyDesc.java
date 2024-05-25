fdb7c29346a864c8b8af7630bd36ed92a52a9811
KenishaJazlyn <kenishajazlyn@gmail.com>
2024-05-25 19:28:36 +0700

    [GREEN] Implement sortbyratingdesc strategy

---
+++ src/main/java/id/ac/ui/cs/advprog/farrel/strategy/SortByRatingStrategyDesc.java
@@ -0,0 +1,15 @@
+
+package id.ac.ui.cs.advprog.farrel.strategy;
+import id.ac.ui.cs.advprog.farrel.model.Review;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortByRatingStrategyDesc implements SortStrategy {
+    @Override
+    public List<Review> sort(List<Review> reviews) {
+        Collections.sort(reviews, Comparator.comparingInt(Review::getRating).reversed());
+        return reviews;
+    }
+}
