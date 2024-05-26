18ac567f5011b639226870adebe402992cf50a1b
DarilRaf <darilmrafansyah@gmail.com>
2024-05-26 20:02:07 +0700

    [REFACTOR] Added WebConfiguration to bypass CORS

---
+++ src/main/java/id/ac/ui/cs/advprog/farrel/config/WebConfiguration.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.farrel.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
